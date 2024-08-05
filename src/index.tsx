546bbe80675e5daa06ef6ac8a3bd80ff97e0f22b
Ben Zhong <benzhong@Bens-MBP.hsd1.va.comcast.net>
2024-08-05 01:16:27 -0400

    deployed

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
 const rootElement = document.getElementById('root')!
 
 ReactDOM.createRoot(rootElement).render(
